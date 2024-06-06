99e6bd53396446cfb01181ec586c9cfd2cf35a2f
Eliel Alves <elielalves.dev@gmail.com>
2024-06-06 18:45:34 -0300

    feat: add transaction table and json server

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,41 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton defaultChecked value="income" variant="income" type="button">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton value="outcome" variant="outcome" type="button">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}
